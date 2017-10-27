728747ffb8f3f54af8a001ff1d99453efedc58ee
Scott Shuffler <scottshuffler@gmail.com>
2017-10-27 16:43:04 -0400

    init

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export default class App extends React.Component {
+
+    sum(a, b) {
+        return a + b;
+    }
+
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>);
+  }
+}
---
+++ src/components/app.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from './app';
+import Sum from './sum';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(App.prototype.sum(1, 2)).toBe(3);
+});
